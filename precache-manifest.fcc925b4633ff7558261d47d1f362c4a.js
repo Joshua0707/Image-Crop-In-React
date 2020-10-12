963070153ebfa4e9929ab1c6d58d7d025a98a1c5
Joshua0707 <oladapoodelola@gmail.com>
2020-10-12 11:38:04 +0100

    Updates

---
+++ precache-manifest.fcc925b4633ff7558261d47d1f362c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e582d9f7683f17bc1fa191ec8c5a4947",
+    "url": "/Image-Crop-In-React/index.html"
+  },
+  {
+    "revision": "bef1c06798f25c49eadc",
+    "url": "/Image-Crop-In-React/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "e96ea13bf385eb468568",
+    "url": "/Image-Crop-In-React/static/css/main.4f71de25.chunk.css"
+  },
+  {
+    "revision": "bef1c06798f25c49eadc",
+    "url": "/Image-Crop-In-React/static/js/2.d0462df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Crop-In-React/static/js/2.d0462df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96ea13bf385eb468568",
+    "url": "/Image-Crop-In-React/static/js/main.f0c4a209.chunk.js"
+  },
+  {
+    "revision": "5a8a267a793fcb0edc93",
+    "url": "/Image-Crop-In-React/static/js/runtime-main.22c19e14.js"
+  }
+]);
